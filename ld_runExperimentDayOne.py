00ead91e32e95653a2adfd1ef464dc43fe094dcb
ThibaultVlieghe <thibault.vlieghe@mcgill.ca>
2021-04-13 07:56:42 -0400

    this is how we will get the language for now, language has yet to refresh within the menu. There seems to be a bug for some subject names, where some subject names get the lanuage of other subjects. Will investigate and fix

--- ld_runExperimentDayOne.py
+++ ld_runExperimentDayOne.py
@@ -3,16 +3,57 @@ from cursesmenu.items import *
 import sys
 import os
 
+from datetime import datetime
+from expyriment import misc
+from dateutil.parser import parse
+
+
+rawFolder = os.getcwd() + os.path.sep
+dataFolder = rawFolder + 'data' + os.path.sep
+
+
+def getLanguage(subjectName, daysBefore, experienceName):
+    currentDate = datetime.now()
+    dataFiles = [file for file in os.listdir(dataFolder) if file.endswith('.xpd')]
+
+    output = None
+
+    for dataFile in dataFiles:
+        agg = misc.data_preprocessing.read_datafile(dataFolder + dataFile, only_header_and_variable_names=True)
+        previousDate = parse(agg[2]['date'])
+
+        try:
+            agg[3].index(experienceName)
+        except ValueError:
+            continue
+        if daysBefore == 0 or ((currentDate-previousDate).total_seconds() > 72000*daysBefore and (currentDate-previousDate).total_seconds() < 100800*daysBefore):
+            header = agg[3].split('\n#e ')
+
+            indexSubjectName = header.index('Subject:') + 1
+            if subjectName in header[indexSubjectName]:
+                print('File found: ' + dataFile)
+                indexPositions = header.index('language:') + 1
+                language = header[indexPositions].split('\n')[0].split('\n')[0]
+                output = language
+
+    # This ensures the latest ld
+    return output
+
+
+# subjectName = sys.argv[1]
+subjectName = 'test'
+
+language = str(getLanguage(subjectName, 0, 'choose-language'))
+# 'None' if no languages were chosen previously, said language otherwise, e.g. 'french'
 
 python = 'py'
-language = 'None'
 
 # Create the menu
 menu = CursesMenu(title="Declarative Task - Day One", subtitle='Subject: ' + sys.argv[1] + ' ; language: ' + language)
 
 dayOneChooseLanguage = CommandItem(text='choose language',
                             command=python + " src" + os.path.sep + "ld_choose_language.py",
-                            arguments='choose-language, ' + sys.argv[1] + ', ' + language,
+                            arguments='choose-language, ' + sys.argv[1] + ', ' + 'None',
                             menu=menu,
                             should_exit=False)
 
