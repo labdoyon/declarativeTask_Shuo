59780e0ce20a9d80c2ca99cfee9cb3b4fb1ff68f
ThibaultVlieghe <thibault.vlieghe@mcgill.ca>
2021-04-15 20:34:45 -0400

    src/stimulti_presentation.py finished; more regarding language management to be added later

--- src/ld_stimuli_presentation.py
+++ src/ld_stimuli_presentation.py
@@ -31,9 +31,15 @@ exp = design.Experiment(experimentName)  # Save experiment name
 exp.add_experiment_info('Subject: ')
 exp.add_experiment_info(subjectName)  # Save Subject Code
 
+# Save time, nblocks, position, correctAnswer, RT
+exp.add_data_variable_names(['show_or_hide', 'Time', 'category', 'Picture', 'picture_name'])
+
 # save image categories used for experiment
 exp.add_experiment_info('Image categories (original order; src/config.py order): ')
 exp.add_experiment_info(str(classPictures))
+
+exp.add_experiment_info('pictures\'s list: ')
+exp.add_experiment_info(str(listPictures))
 
 # Check WindowMode and Resolution
 if not windowMode:
@@ -53,7 +59,8 @@ control.start(exp, auto_create_subject_id=True, skip_ready_screen=True)
 
 # randomising presentation order of the categories
 classPicturesPresentationOrder = list(np.random.permutation(classPictures))
-exp.add_experiment_info('presentation order (Image categories): ' + str(classPicturesPresentationOrder))
+exp.add_experiment_info('presentation order (Image categories): ')
+exp.add_experiment_info(str(classPicturesPresentationOrder))
 
 # Graphical instructions
 bs = stimuli.BlankScreen(bgColor)  # Create blank screen
@@ -63,19 +70,19 @@ instructionRectangle = stimuli.Rectangle(size=(windowSize[0], m.gap * 2 + cardSi
 
 
 def create_instructions_box(box_text):
-    box = stimuli.TextLine(box_text,
+    instructions_box = stimuli.TextLine(box_text,
                            position=(0, -windowSize[1]/float(2) + (2*m.gap + cardSize[1])/float(2)),
                            text_font=None, text_size=textSize, text_bold=None, text_italic=None,
                            text_underline=None, text_colour=textColor,
                            background_colour=bgColor,
                            max_width=None)
-    return box
+    return instructions_box
 
 
-def show_and_hide_text_box(background, text_box, onscreen_time, just_show=False, just_hide=False):
+def show_and_hide_text_box(background, instructions_box, onscreen_time, just_show=False, just_hide=False):
     if not just_hide:
         instructionRectangle.plot(background)
-        text_box.plot(background)
+        instructions_box.plot(background)
         background.present(False, True)
         exp.clock.wait(onscreen_time)  # Short Rest between presentation and cue-recall
     if not just_show:
@@ -83,6 +90,7 @@ def show_and_hide_text_box(background, text_box, onscreen_time, just_show=False,
         background.present(False, True)
 
 
+exp.add_experiment_info('Presentation start')
 instructions = create_instructions_box(' PRESENTATION: PRESENTING ALL STIMULIS ')
 show_and_hide_text_box(bs, instructions, shortRest)
 
@@ -96,6 +104,7 @@ for category in classPicturesPresentationOrder:
     # randomise pictures' presentation order
     category_pictures = np.random.permutation(category_pictures)
 
+    exp.add_experiment_info(' PRESENTATION: PRESENTING CATEGORY ' + category)
     instructions = create_instructions_box(' PRESENTATION: PRESENTING CATEGORY ' + category.upper() + ' ')
     show_and_hide_text_box(bs, instructions, shortRest)
 
@@ -111,9 +120,15 @@ for category in classPicturesPresentationOrder:
         m.plotCueCard(True, bs, True)  # Show Cue
         instructions = create_instructions_box(picture_title)
         show_and_hide_text_box(bs, instructions, 0, just_show=True, just_hide=False)
+        exp.add_experiment_info(
+            'ShowCard_pos_{}_card_{}_name_{}_timing_{}'.format('None', picture, picture_title, exp.clock.time))
+        exp.data.add(['show', exp.clock.time, category, picture, picture_title])
         exp.clock.wait(presentationCard)  # Wait presentationCard
         show_and_hide_text_box(bs, instructions, 0, just_show=False, just_hide=True)
         m.plotCueCard(False, bs, True)  # Hide Cue
+        exp.add_experiment_info(
+            'HideCard_pos_{}_card_{}_name_{}__timing_{}'.format('None', picture, picture_title, exp.clock.time))
+        exp.data.add(['hide', exp.clock.time, category, picture, picture_title])
 
         ISI = design.randomize.rand_int(min_max_ISI[0], min_max_ISI[1])
         exp.clock.wait(ISI)
